1318cab801b897e43650cf4b7580a929445ee7cd
Piotrek Bator <piotrek@nibynic.com>
2021-11-12 15:53:26 +0100

    Version bumped to 0.6.7

--- src/lemonink.php
+++ src/lemonink.php
@@ -4,11 +4,11 @@
  * Plugin Name: LemonInk Ebook Watermarking for WooCommerce
  * Plugin URI: https://www.lemonink.co/
  * Description: Watermark EPUB, MOBI and PDF files in your WooCommerce store using the LemonInk service.
- * Version: 0.6.6
+ * Version: 0.6.7
  * Author: LemonInk
  * Author URI: https://www.lemonink.co/
  * Requires at least: 4.4
- * Tested up to: 5.6
+ * Tested up to: 5.8
  *
  * Text Domain: lemonink
  * Domain Path: /languages/
--- src/readme.txt
+++ src/readme.txt
@@ -2,8 +2,8 @@
 Contributors: lemonink
 Tags: lemonink, ecommerce, e-commerce, downloadable, downloads, ebooks, drm, watermark, watermarking, epub, mobi, pdf, Kindle, woocommerce
 Requires at least: 4.4
-Tested up to: 5.6
-Stable tag: 0.6.6
+Tested up to: 5.8
+Stable tag: 0.6.7
 License: MIT
 
 Watermark EPUB, MOBI and PDF files in your WooCommerce store using the LemonInk service.
